9963c5670a5f6f69a36c0b37d2389173f2020172
jhh1988 <hill5637@vandals.uidaho.edu>
2018-02-20 18:45:06 -0800

    Add files via upload

---
+++ src/henry/hh_main_player.cpp
@@ -0,0 +1,25 @@
+#include "hh_main_player.h"
+
+hh_main_player::hh_main_player()
+{
+
+}
+
+void Player::keyPressEvent(QKeyEvent *event)
+{
+    if( event->key() == Qt::Key_Left)
+    {
+     //move left
+    }
+else if( event->key() == Qt::Key_Right)
+    {
+     //move right
+    }
+else if( event->key() == Qt::Key_Up)
+    {
+//move up
+    }
+else if( event->key() == Qt::Key_Down)
+    {
+    //move down
+    }
