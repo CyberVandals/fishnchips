f317b3fdbcf8beb99fbebdee265d5dc55ee8ee23
jhh1988 <hill5637@vandals.uidaho.edu>
2018-02-21 22:34:44 -0800

    added updated player

--- inc/henry/hh_main_player.h
+++ inc/henry/hh_main_player.h
@@ -6,16 +6,23 @@
 #include <QWidget>
 #include <QObject>
 #include <QGraphicsRectItem>
+#include <QDesktopWidget>
+#include <QTimer>
+#include <QGraphicsScene>
+#include <QApplication>
 
 class Main_player: public QObject, public QGraphicsRectItem
 {
 Q_OBJECT
 private:
-
+bool has_banana;
 
 public:
     Main_player();
     void keyPressEvent(QKeyEvent * event);
+
+public slots:
+    void sink();
 };
 
 #endif // HH_MAIN_PLAYER_H
