bbe0721d7317ee00bb7ef273a39440b96acae97c
Matthew <mtthw.mlls@gmail.com>
2018-02-21 18:04:34 -0800

    Adding files

---
+++ inc/matthew/soundmanager.h
@@ -0,0 +1,20 @@
+#ifndef SOUNDMANAGER_H
+#define SOUNDMANAGER_H
+#include <QList>
+
+class SoundManager
+{
+private:
+
+    QList<QString> resourcePathList;
+
+    struct files{
+
+
+    };
+
+public:
+    SoundManager();
+};
+
+#endif // SOUNDMANAGER_H
