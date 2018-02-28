6bf88c5e1f50ef784e10004ef729cfc110635b6a
jhh1988 <hill5637@vandals.uidaho.edu>
2018-02-27 21:08:06 -0800

    adding player health

--- src/henry/hh_health_bar.cpp
+++ src/henry/hh_health_bar.cpp
@@ -17,6 +17,8 @@ void Health::renew_health()
 HealthBar::HealthBar()
 {
     Health * new_health = tail;
+    new_health = new Health(0,0 50,50);
+
     for(int i = 0; i < 5; i++)
     {
         new_health = new Health(this->pos().x()+10,this->pos().y(),this)
