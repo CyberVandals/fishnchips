85198078d93d674b03a446d85ad505311b3193f8
Chaeun Kim <chaeunkim@codnsdlaorqnr.local>
2018-03-05 00:33:01 -0800

    files edited by chae

---
+++ inc/chaeunkim/ck_pausescreen.h
@@ -0,0 +1,14 @@
+#ifndef CK_PAUSESCREEN_H
+#define CK_PAUSESCREEN_H
+
+#include <QGraphicsTextItem>
+
+class PauseScreen : public QGraphicsTextItem{
+
+public:
+    PauseScreen();
+    void bring_pauseScreen();
+
+};
+
+#endif // CK_PAUSESCREEN_H
---
+++ src/chaeunkim/ck_pausescreen.cpp
@@ -0,0 +1,13 @@
+#include "../../inc/chaeunkim/ck_pausescreen.h"
+#include <QDebug>
+
+PauseScreen::PauseScreen(){//QGraphicsScene * scene){
+    this->setHtml(QString("<div style='background:rgba(45, 45, 36, 100%);'>" + QString("PAUSE") + QString("</div>") ));
+    this->setPos(500,500);
+    this->setTextWidth(500);
+
+}
+void PauseScreen::bring_pauseScreen(){
+    //setHtml(QString("<div style='background:rgba(255, 255, 255, 100%);'>" + QString("put your text here") + QString("</div>") );
+
+}
