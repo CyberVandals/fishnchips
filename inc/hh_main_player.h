a1c143625caf06a15a5855ec09f07de876821a9f
jhh1988 <hill5637@vandals.uidaho.edu>
2018-03-04 20:41:54 -0800

    updated file path

--- inc/hh_main_player.h
+++ inc/hh_main_player.h
@@ -13,8 +13,8 @@
 #include <QGraphicsItem>
 #include <QList>
 #include <typeinfo>
-#include "../../inc/henry/hh_health.h"
-#include "../../inc/henry/hh_health_bar.h"
+#include "henry/hh_health.h"
+#include "henry/hh_health_bar.h"
 
 class Main_player: public QObject, public QGraphicsRectItem
 {
