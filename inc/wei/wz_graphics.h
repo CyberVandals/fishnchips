cbe1eb339c661f4d80bc0b51536ecea359a56fa6
Wei Zhao <zhao3940@vandals.uidaho.edu>
2018-03-04 20:38:32 -0800

    Add files via upload

--- inc/wei/wz_graphics.h
+++ inc/wei/wz_graphics.h
@@ -3,21 +3,25 @@
 
 #include <QGraphicsPixmapItem>
 #include "../henry/hh_main_player.h"
-//#include "../gigiyoung/gy_object.h"
 #include "../gigiyoung/gy_object.h"
 #include <QGraphicsPathItem>
 
 class Shark;
+class Banana;
+class Platform;
+class Steam;
+
 class Graphics : public QGraphicsPathItem {
 public:
     //void load_mainPlayer(Main_player *player);
     Graphics();
     void load_shark (int width,int heigh,Shark *shark);
     void load_shark (Shark *shark);
-    /*void load_banana(Banana *banana);
-    void load_platform(Platform *Platform);
-    void load_steam(Steam *steam);
-    */
+    void load_banana(Banana *banana);
+    void load_banana(int width,int heigh,Banana *banana);
+    //void load_platform(Platform *Platform);
+    //void load_steam(Steam *steam);
+
 private:
 };
 
