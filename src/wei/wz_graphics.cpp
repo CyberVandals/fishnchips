6735a38bb8a103e262512b38a52954975d8e2a15
Wei Zhao <zhao3940@vandals.uidaho.edu>
2018-03-04 20:39:03 -0800

    Add files via upload

--- src/wei/wz_graphics.cpp
+++ src/wei/wz_graphics.cpp
@@ -1,5 +1,7 @@
 #include "../../inc/wei/graphics.h"
 #include "../../inc/gigiyoung/gy_object.h"
+
+
 /*void Graphics::load_main_player(Main_player *player)
 {
     player->setPixmap(QPixmap(":/Object_image/monkey.png"));
@@ -13,20 +15,38 @@ void Graphics::load_shark(Shark *shark)
 }*/
 void Graphics::load_shark(Shark *shark)
 {
-    shark->setPixmap(QPixmap(":/resources/images/blue_shark.png"));
+    shark->setPixmap(QPixmap(":/images/blue_shark.png"));
     /*QPixmap pixmap(":/steam.png");
     pixmap = pixmap.scaled(width,heigh,Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
     shark->setPixmap(pixmap);
      */
 
 }
+
+
+
 void Graphics::load_shark(int width,int heigh,Shark *shark)
 {
     //shark->setPixmap(QPixmap(":/blue_shark.png"));
-    QPixmap pixmap(":/resources/images/blue_shark.png");
+    QPixmap pixmap(":/images/blue_shark.png");
     pixmap = pixmap.scaled(width,heigh,Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
     shark->setPixmap(pixmap);
     
+}
+void Graphics::load_banana(Banana *banana)
+{
+     banana->setPixmap(QPixmap(":/images/banana.png"));
+}
+
+void Graphics:: load_banana(int width,int heigh,Banana *banana)
+{
+    QPixmap pixmap(":/images/banana.png");
+    pixmap = pixmap.scaled(width,heigh,Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+    banana->setPixmap(pixmap);
+}
+Graphics::Graphics()
+{
+    //
 }
 
 /*
