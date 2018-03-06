0d94cfa762ec6cf3fcd82ce6e3fdbea26f348ff4
Gigi <iratewaffle@gmail.com>
2018-03-06 08:04:50 -0800

    updated test route

--- src/gigiyoung/gy_autotest.cpp
+++ src/gigiyoung/gy_autotest.cpp
@@ -32,14 +32,13 @@ AutoTest::AutoTest(QGraphicsScene *scene, QGraphicsItem *object, SceneManager *s
     // add test points
     add_point(QPoint(300,400));
     add_point(QPoint(500,0));
-    add_point(QPoint(50,50));
-    add_point(QPoint(50,50));
-    add_point(QPoint(50,50));
+    add_point(QPoint(200,0));
+    add_point(QPoint(100,60));
     add_point(QPoint(600,50));
 
 
     // if no player object passed, then create a demo level
-    if(object == 0)
+    if(object == 0 && sm == 0)
         create_demo();
 
     connect(timer, SIGNAL(timeout()), this, SLOT(simulate_keypress()));
@@ -55,17 +54,40 @@ bool AutoTest::change_scene(QGraphicsScene *scene) {
 }
 
 void AutoTest::create_demo() {
+
+//    QGraphicsItem *player, *shark1, *shark2, *shark3, *shark4, *shark5, *plat1, *plat2, *plat3, *bana1, *steam1, *exit;
     Main_player *player;
     Shark *shark1, *shark2, *shark3, *shark4, *shark5;
-    Platform *plat1, *plat2, plat3;
+    Platform *plat1, *plat2, *plat3;
     Banana *bana1;
     Steam *steam1;
     Exit *exit;
 
     player = new Main_player(receiver);
  
-    shark1 = new Shark();
+    shark1 = new Shark(30,30,5,5);
+    shark2 = new Shark(50,30,10,0);
+    shark3 = new Shark(100,30,3,7);
+    shark4 = new Shark(300,500,10,0);
+    shark5 = new Shark(500,30,10,0);
 
+    plat1 = new Platform(500,10,0,200);
+    plat2 = new Platform(500,10,200,400);
+    plat3 = new Platform(500,10,100,600);
+
+    exit = new Exit();
+
+    receiver->addItem(shark1);
+    receiver->addItem(shark2);
+    receiver->addItem(shark3);
+    receiver->addItem(shark4);
+    receiver->addItem(shark5);
+
+    receiver->addItem(plat1);
+    receiver->addItem(plat2);
+    receiver->addItem(plat3);
+
+    receiver->addItem(exit);
 }
 
 void AutoTest::restart_scene() {
@@ -102,8 +124,8 @@ void AutoTest::simulate_keypress() {
     //QKeyEvent key_event;
     int x = object->x();
     int y = object->y();
-
-    //qDebug() << "simulate_keypress()";
+    
+    qDebug() << x << ", " << y;
 
     // init if self.points isn't empty
     if( (dest.x() == -1 && dest.y() == -1) && points.size() > 0 )
@@ -117,12 +139,12 @@ void AutoTest::simulate_keypress() {
 
     // left or right
     if( x != dest.x() ) {
-        if( x < dest.x() ) {
+        if( x < dest.x()-5 ) {
             //object->setPos(x+10, y);
             QKeyEvent event(QEvent::KeyPress, Qt::Key_Right, Qt::NoModifier);
             QCoreApplication::sendEvent(receiver, &event);
         }
-        else if( x > dest.x() ) {
+        else if( x > dest.x()+5 ) {
             //object->setPos(x-10, y);
             QKeyEvent event(QEvent::KeyPress, Qt::Key_Left, Qt::NoModifier);
             QCoreApplication::sendEvent(receiver, &event);
@@ -131,12 +153,12 @@ void AutoTest::simulate_keypress() {
 
    // up or down
     if( y != dest.y() ) {
-        if( y < dest.y() ) {
+        if( y < dest.y()-5 ) {
             //object->setPos(x, y+10);
             QKeyEvent event(QEvent::KeyPress, Qt::Key_Down, Qt::NoModifier);
             QCoreApplication::sendEvent(receiver, &event);
         }
-        else if( y > dest.y() ) {
+        else if( y > dest.y()+5 ) {
             //object->setPos(x, y-10);
             QKeyEvent event(QEvent::KeyPress, Qt::Key_Up, Qt::NoModifier);
             QCoreApplication::sendEvent(receiver, &event);
@@ -144,8 +166,8 @@ void AutoTest::simulate_keypress() {
             
     }
         
-    if( (x < dest.x()+5 && x > dest.x()-5) && 
-        (y < dest.y()+5 && y > dest.y()-5) &&
+    if( (x < dest.x()+10 && x > dest.x()-10) && 
+        (y < dest.y()+10 && y > dest.y()-10) &&
         list_pos < points.size()-1 )
     {
         list_pos++;
