92324ad1f629e315cb17b253870aec1af90d7051
Chaeun Kim <chaeunkim@codnsdlaorqnr.local>
2018-04-05 09:46:35 -0700

    edited some stuff

--- src/chaeunkim/ck_button.cpp
+++ src/chaeunkim/ck_button.cpp
@@ -1,72 +1,63 @@
 #include "../../inc/chaeunkim/ck_button.h"
 #include <QPushButton>
 #include <QBrush>
-#include <QGraphicsTextItem>
 #include <QIcon>
 
-Button::Button(QString path, QPushButton *parent) : QPushButton(parent)
+Button::Button( QString path, QPushButton *parent )
+    : QPushButton(parent)
 {
     QPixmap pixmap(path);
     QIcon ButtonIcon(pixmap);
     this->setIcon(ButtonIcon);
-    this->setFixedSize(150,100);
-    this->setIconSize(QSize(150,100));
+    this->setFixedSize(DEFAULT_BTN_WIDTH, DEFAULT_BTN_HEIGHT);
+    this->setIconSize(QSize(DEFAULT_BTN_WIDTH, DEFAULT_BTN_HEIGHT));
     this->setStyleSheet("QPushButton{background: transparent;}");
     // allow tracking mouse for mouse hovering effect
     setMouseTracking(true);
     setAttribute(Qt::WA_Hover);
-
-
 }
 
-void Button::hoverEnter(QHoverEvent *)
-{
-    int newW = this->width() + 5;
-    int newH = this->height() + 5;
-    this->setIconSize(QSize(newW,newH));
-
+void Button::hoverEnter( QHoverEvent * ) {
+    new_width = this->width() + 5;
+    new_height = this->height() + 5;
+    this->setIconSize(QSize(new_width,new_height));
 }
 
-void Button::hoverLeave(QHoverEvent *)
-{
-    int newW = this->width() - 5;
-    int newH = this->height() - 5;
-    this->setIconSize(QSize(newW,newH));
+void Button::hoverLeave( QHoverEvent * ) {
+    new_width = this->width() - 5;
+    new_height = this->height() - 5;
+    this->setIconSize(QSize(new_width,new_height));
 }
 
-void Button::hoverMove(QHoverEvent *)
-{
-    int newW = this->width() + 5;
-    int newH = this->height() + 5;
-    this->setIconSize(QSize(newW,newH));
+void Button::hoverMove( QHoverEvent * ) {
+    new_width = this->width() + 5;
+    new_height = this->height() + 5;
+    this->setIconSize(QSize(new_width,new_height));
 
 }
-void Button::mousePressEvent(QMouseEvent *)
-{
+void Button::mousePressEvent( QMouseEvent * ) {
     emit clicked();
 }
-bool Button::event(QEvent *event)
-{
-    switch(event->type())
-    {
-    case QEvent::HoverEnter:
-        hoverEnter(static_cast<QHoverEvent*>(event));
-        return true;
-        break;
-    case QEvent::HoverLeave:
-        hoverLeave(static_cast<QHoverEvent*>(event));
-        return true;
-        break;
-    case QEvent::HoverMove:
-        hoverMove(static_cast<QHoverEvent*>(event));
-        return true;
-        break;
-    case QEvent::MouseButtonPress:
-        mousePressEvent(static_cast<QMouseEvent*>(event));
-        return true;
-        break;
-    default:
-        break;
+bool Button::event( QEvent *event ) {
+    switch( event->type() ) {
+        case QEvent::HoverEnter:
+            hoverEnter(static_cast<QHoverEvent*>(event));
+            return true;
+            break;
+        case QEvent::HoverLeave:
+            hoverLeave(static_cast<QHoverEvent*>(event));
+            return true;
+            break;
+        case QEvent::HoverMove:
+            hoverMove(static_cast<QHoverEvent*>(event));
+            return true;
+            break;
+        case QEvent::MouseButtonPress:
+            mousePressEvent(static_cast<QMouseEvent*>(event));
+            return true;
+            break;
+        default:
+            break;
     }
     return QWidget::event(event);
 }
