6b43d699cb62ba13f87bd7cac451c3189eb5a32d
Gigi <iratewaffle@gmail.com>
2018-05-01 19:40:58 -0700

    fixes

--- src/gigiyoung/gy_objecthandler.cpp
+++ src/gigiyoung/gy_objecthandler.cpp
@@ -128,50 +128,50 @@ void ObjectHandler::resume() {
 }
 
 void ObjectHandler::remove_all() {
-    int size;
 
-    // remove from scene and deallocate
-    for( int i = 0; i < shark_list.size(); i++ ) {
-        shark_list[i]->setVisible(false); 
-        shark_list[i]->setEnabled(false); 
-         
-        scene->removeItem(shark_list[i]); 
-        delete shark_list[i];
+    for( int i = 0; i < platform_list.size(); i++ ) {
+        if( platform_list[i] != NULL ) {
+            scene->removeItem(platform_list[i]); 
+            delete platform_list[i];
+            platform_list[i] = NULL;
+        }
     }
-
-    for( int i = 0; i < shark_list.size(); i++ ) {
-        steam_list[i]->setVisible(false); 
-        steam_list[i]->setEnabled(false); 
-        scene->removeItem(steam_list[i]); 
-        delete steam_list[i];
-    }
+    while( !platform_list.isEmpty() ) 
+        platform_list.removeLast();
 
     for( int i = 0; i < shark_list.size(); i++ ) {
-        banana_list[i]->setVisible(false); 
-        banana_list[i]->setEnabled(false); 
-        scene->removeItem(banana_list[i]);  
-        delete banana_list[i];
+        if( shark_list[i] != NULL ) {
+            scene->removeItem(shark_list[i]); 
+            delete shark_list[i];
+            shark_list[i] = NULL;
+        }
     }
+    while( !shark_list.isEmpty() ) 
+        shark_list.removeLast();
+
+    for( int i = 0; i < steam_list.size(); i++ ) {
+        if( steam_list[i] != NULL ) {
+            scene->removeItem(steam_list[i]); 
+            delete steam_list[i];
+            steam_list[i] = NULL;
+        }
+    }
+    while( !steam_list.isEmpty() ) 
+        steam_list.removeLast();
+
+    for( int i = 0; i < banana_list.size(); i++ ) {
+        if( banana_list[i] != NULL ) {
+            scene->removeItem(banana_list[i]);  
+            delete banana_list[i];
+            banana_list[i] = NULL;
+        }
+    }
+    while( !banana_list.isEmpty() ) 
+        banana_list.removeLast();
 
     if( exit != NULL ) {
-        exit->setVisible(false); 
-        exit->setEnabled(false); 
         scene->removeItem(exit);
         delete exit;
         exit = NULL;
     }
-
-    // clear pointer lists 
-    while( !shark_list.isEmpty() ) 
-        shark_list.removeLast();
-
-    while( !steam_list.isEmpty() ) 
-        steam_list.removeLast();
-
-    while( !banana_list.isEmpty() ) 
-        banana_list.removeLast();
-
-    while( !platform_list.isEmpty() ) 
-        platform_list.removeLast();
-
 }
